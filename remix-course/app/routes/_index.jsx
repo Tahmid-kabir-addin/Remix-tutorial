321b473f14fb33d894abeb72025086a1ba1b09c5
Tahmid-kabir-addin <tahmidkabiraddin@gmail.com>
2024-01-03 00:13:26 +0600

    note app project started

--- remix-course/app/routes/_index.jsx
+++ remix-course/app/routes/_index.jsx
@@ -1,15 +1,13 @@
-import { Link } from "@remix-run/react";
-
+import { Link } from '@remix-run/react';
 
 export default function Index() {
   return (
-    <>
-    <h1>Hello World</h1>
-    <a href="/demo">Go to Demo Page</a>
-    <div>
-
-    <Link to="/demo">Go to Downloaded Demo page</Link>
-    </div>
-    </>
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our early beta and never loose track of your notes!</p>
+      <p id="cta">
+        <Link to = "/notes">Try Out!</Link>
+      </p>
+    </main>
   );
 }
